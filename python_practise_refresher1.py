7806f443ecd82459df5277d2a2c7c9e7b6f2e8c2
Bits A <satyam29aggarwal@gmail.com>
2026-07-03 00:50:13 +0530

    python refresher 1 practise

---
+++ python_practise_refresher1.py
@@ -0,0 +1,19 @@
+# x = -1
+# print("Logical not: ", not x)
+# x = not x
+# print("value of x: ", x)
+
+
+#bitwise operators
+# a = 2
+# print(f"Decimal: {a} -> Binary: {a:08b}") 
+# print("Bitwise not: ", ~a)
+
+# b = 0
+# print (b >> 1)
+
+# Dictionary
+
+a = {'a' : 1, 10 : 3}
+print(a)
+print(type(a))
